6411f8addab181cda927d411951a760295e7d6dc
shivadasari88 <dasarishivasai71@gmail.com>
2025-12-12 11:58:10 +0530

    rest demo with jpa database connection

---
+++ SPRING-REST/springbootrest-1/src/test/java/com/shiva/Springbootrest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootrest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
