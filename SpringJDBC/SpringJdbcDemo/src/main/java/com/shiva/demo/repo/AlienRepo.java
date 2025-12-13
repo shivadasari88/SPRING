5f7c122e472df99c619e9a6ca2112ed9b41a821c
shivadasari88 <dasarishivasai71@gmail.com>
2025-12-13 07:52:55 +0530

    stored data and retrived the data of the internal h2 db using the JdbcTemplate

--- SpringJDBC/SpringJdbcDemo/src/main/java/com/shiva/demo/repo/AlienRepo.java
+++ SpringJDBC/SpringJdbcDemo/src/main/java/com/shiva/demo/repo/AlienRepo.java
@@ -1,18 +1,50 @@
 package com.shiva.demo.repo;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import com.shiva.demo.model.Alien;
 
 @Repository
 public class AlienRepo {
-	public void save(Alien alien) {
-		System.out.println("Added");
+	
+	private JdbcTemplate template;
+	
+	public JdbcTemplate getTemplate() {
+		return template;
+	}
+	@Autowired  //it creates the object for the template
+	public void setTemplate(JdbcTemplate template) {
+		this.template = template;
+	}
+	public void save(Alien alien) { 
+		String sql = "insert into alien (id, name, tech) values(?,?,?)";
+		template.update(sql, alien.getId(),alien.getName(),alien.getTech());
 	}
 	public List<Alien> findAll(){
-		return new ArrayList<Alien>();
+		
+		String sql = "select * from alien";
+		
+		RowMapper<Alien> mapper = new RowMapper<Alien>() { //this will generate the table as row by row with coloumn nuber
+			@Override
+			public Alien mapRow(ResultSet rs, int rowNum) throws SQLException{
+				Alien a = new Alien();
+				a.setId(rs.getInt(1));
+				a.setName(rs.getString(2));
+				a.setTech(rs.getString(3));
+				
+				return a;
+			}
+		};
+		
+		List<Alien> aliens = template.query(sql, mapper);
+		return aliens;
 	}
 }
---
+++ SpringJDBC/SpringJdbcDemo/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into alien (id, name, tech) values (101, 'kiran', 'Ai')
+insert into alien (id, name, tech) values (102, 'kan', 'ewbai')
+insert into alien (id, name, tech) values (103, 'kir', 'dev')
