18ccfc012564ef7768915747bf09745ca0944731
shivadasari88 <dasarishivasai71@gmail.com>
2025-12-13 12:12:39 +0530

    securing the jsp file to access only from the controller
    added the siffix and prefix in properties file

--- SpringMVC/demo/src/main/java/com/shiva/springmvcboot/HomeController.java
+++ SpringMVC/demo/src/main/java/com/shiva/springmvcboot/HomeController.java
@@ -13,14 +13,14 @@ public class HomeController {
 	
 	@RequestMapping("/")
 	public String home() {
-		return "index.jsp";
+		return "index";
 	}
 	
 	@RequestMapping("add")
 	public ModelAndView add(@RequestParam("num1") int i,  @RequestParam("num2") int j) {
 		
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("result.jsp");
+		mv.setViewName("result");
 		
 		
 		int num3 = i+j;
