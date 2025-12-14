62023d2c85ab994c5a78fdb0e1bf28d7703fac02
shivadasari88 <dasarishivasai71@gmail.com>
2025-12-14 12:37:02 +0530

    pathvarible and post mapping

--- SpringMVC/demo/src/main/java/com/shiva/springmvcboot/AlienController.java
+++ SpringMVC/demo/src/main/java/com/shiva/springmvcboot/AlienController.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.shiva.springmvcboot.model.Alien;
 
@@ -22,4 +25,21 @@ public class AlienController {
 		List<Alien> aliens = repo.findAll();
 		return aliens;
 	}
+	
+	@GetMapping("alien/{aid}")
+	@ResponseBody
+	public Alien getAlien(@PathVariable("aid") int aid) {
+		Alien alien = repo.findById(aid).orElse(new Alien(0,""));
+		return alien;
+	}
+	
+	
+	@PostMapping("alien") 
+    public Alien addAlien( Alien alien) {
+        
+        repo.save(alien);
+        
+        return alien;
+    }
+	
 }
